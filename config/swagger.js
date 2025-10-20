f17000e2f47181058dc2b7ce61e043d8ccbf9105
santismith <santii.smiith@hotmail.com>
2025-10-20 17:03:05 -0300

    Se corrige Swagger por los YAML

--- config/swagger.js
+++ config/swagger.js
@@ -1,26 +1,39 @@
 // swagger.js
 const swaggerJSDoc = require('swagger-jsdoc');
-const userSwagger = require('./usersSwagger'); 
-const commentsSwagger = require('./commentsSwagger'); 
+
 const options = {
-  definition: {
-    openapi: "3.0.0",
+ definition: {
+    openapi: '3.0.0',
     info: {
-      title: "API de Anti-Social",
-      version: "1.0.0",
-      description: "Documentación de la API con Swagger",
+      title: 'API de Anti-Social',
+      version: '1.0.0',
+      description: 'Documentación de la API con Swagger',
     },
     servers: [
       {
-        url: "http://localhost:3000", 
+        url: 'http://localhost:3000',
       },
     ],
-       paths: {
-      ...userSwagger, 
-      ...commentsSwagger,
-    },
+    tags: [
+      {
+        name: 'Usuarios',
+        description: 'Operaciones relacionadas con los usuarios y sus seguidores.',
+      },
+      {
+        name: 'Posts',
+        description: 'Endpoints para crear, ver y gestionar posts.',
+      },
+      {
+        name: 'Comentarios',
+        description: 'Endpoints para la gestión de comentarios en los posts.',
+      },
+      {
+        name: 'Tags',
+        description: 'Operaciones para administrar los tags o etiquetas.',
+      },
+    ],
   },
-  apis: ["./src/routes/*.js"], 
+  apis: ['./docs/*.yaml'], 
 };
 
 module.exports = options;
